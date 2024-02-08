af5844cbf4c74322b76032ca6537fe59fbd91b6a
srividya9642 <bellamkondasrividya889771661@gmail.com>
2024-02-08 17:16:00 +0530

    portfolio using react

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const projects =[
+    {
+        title: "React-To-do app",
+        subtitle: "To-Do list web applications",
+        description:
+          "React project to develop a to-do list web applications.",
+        image: "todo.png",
+        link:  ""
+    },
+    {
+        title: "Resume-builder-application",
+        subtitle: "web development project",
+        description:
+          "Resume builder appliaction is developed by using HTML, BOOTSTRAP, JAVASCRIPT",
+        image: "./resume.png",
+        link: ""
+    },
+    {
+        title: "Quiz-app",
+        subtitle: "Front end quiz application",
+        description:
+          "Quiz application is a Frontend web development project developed  using HTML, CSS, JAVASCRIPT ",
+        image: "./quiz.png",
+        linl: ""
+    },
+    {
+        title: "Digital Calculator",
+        subtitle: "calculator to perform mathematical operations",
+        description:
+          "Digital Calculator  is a web application to perform simple mathematical appliacations",
+        image: "./calculator.png",
+        link:  ""  
+    },
+];
+export const skills =[
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Nodejs",
+    "Express",
+    "MongoDB",
+    "MySql",
+    "Python",
+    "React Js"
+];
